1a874fb2340a14f5f21d099afd32bc36b8538835
Isaac Isvaldo Bunga <isaacisvaldobunga300@gmail.com>
2025-06-28 15:11:02 +0100

    feat(API-DEXPRESS): add first configuration

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+    // Configuração da documentação
+  const config = new DocumentBuilder()
+    .setTitle('DExpress')
+    .setDescription('Plataforma online que conecte famílias a profissionais domésticos qualificados em Luanda')
+    .setVersion('1.0')
+    .addBearerAuth() // Se estiver usando JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); // http://localhost:3000/api/docs
+
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
