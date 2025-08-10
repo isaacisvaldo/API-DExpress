0c799229e923a394c19c427d5fe5eb15ed6c25b3
Isaac Isvaldo Bunga <isaacisvaldobunga300@gmail.com>
2025-08-10 15:23:45 +0100

    feat:update data

--- src/module/dashboard/dashboard.service.ts
+++ src/module/dashboard/dashboard.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { CreateDashboardDto } from './dto/create-dashboard.dto';
-import { UpdateDashboardDto } from './dto/update-dashboard.dto';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+
 
 @Injectable()
 export class DashboardService {
-  create(createDashboardDto: CreateDashboardDto) {
-    return 'This action adds a new dashboard';
+   constructor(private readonly prisma: PrismaService) {}
+async getDashboardSummary() {
+const totalProfessionals = await this.prisma.professional.count()
+
+      const data = {
+      totalProfessionals,
+      totalClients: 3,
+      activeServices: 0,
+      canceledRequests: 0,
+    };
+    return data
   }
 
-  findAll() {
-    return `This action returns all dashboard`;
-  }
 
-  findOne(id: number) {
-    return `This action returns a #${id} dashboard`;
-  }
-
-  update(id: number, updateDashboardDto: UpdateDashboardDto) {
-    return `This action updates a #${id} dashboard`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} dashboard`;
-  }
 }
