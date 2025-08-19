24b6119d66429876e180020a303346e1edd01f2b
Isaac Isvaldo Bunga <isaacisvaldobunga300@gmail.com>
2025-08-19 16:17:11 +0100

    feat:update data

---
+++ prisma/migrations/20250818185909_19/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Experience" ADD COLUMN     "description" TEXT DEFAULT '---',
+ADD COLUMN     "endDate" TEXT DEFAULT '---',
+ADD COLUMN     "startDate" TEXT DEFAULT '---';
